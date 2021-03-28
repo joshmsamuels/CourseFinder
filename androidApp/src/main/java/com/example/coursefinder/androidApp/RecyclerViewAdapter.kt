771a938c89662afc455ee1ca916f7bd763e0a5fd
Shani Yona <syona@uoguelph.ca>
2021-03-28 01:41:09 -0400

    search working in recyclerview

--- androidApp/src/main/java/com/example/coursefinder/androidApp/RecyclerViewAdapter.kt
+++ androidApp/src/main/java/com/example/coursefinder/androidApp/RecyclerViewAdapter.kt
@@ -4,34 +4,26 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coursefinder.androidApp.model.CourseView
 
-class RecyclerViewAdapter(private var courses:List<CourseView>): RecyclerView.Adapter<RecyclerViewAdapter.CourseViewHolder>() {
+class RecyclerViewAdapter(var context: Context, private var courses:List<CourseView>): RecyclerView.Adapter<RecyclerViewAdapter.CourseViewHolder>() {
+//    private val courseList = ArrayList<CourseView>()
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
         var itemView = LayoutInflater.from(parent.context).inflate(R.layout.course_list_item, parent, false)
-
-
         val title = itemView.findViewById<TextView>(R.id.course_title)
         val description = itemView.findViewById<TextView>(R.id.course_description)
-
-//        context = parent.context
 
         return CourseViewHolder(itemView, title, description)
     }
 
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
-        var currentItem = courses[position]
-
-        var itemView = holder.itemView
-
-
         holder.itemView.isEnabled = true
-        holder.courseTitle.text = currentItem.title
-        holder.courseDescription.text = currentItem.description
+        holder.courseTitle.text = courses[position].title
+        holder.courseDescription.text = courses[position].description
     }
 
     override fun getItemCount(): Int {
@@ -41,27 +33,10 @@ class RecyclerViewAdapter(private var courses:List<CourseView>): RecyclerView.Ad
     class CourseViewHolder (itemView: View, internal var title:TextView, internal var description:TextView) : RecyclerView.ViewHolder(itemView) {
         var courseTitle: TextView = itemView.findViewById(R.id.course_title)
         var courseDescription:TextView = itemView.findViewById(R.id.course_description)
+    }
 
-        //gets called once for each item that we want to show in recycler view
-        //adds single row of the recycler view
-        fun updateEvent (courseView: CourseView){
-
-
-        }
-
+    fun filterList(filteredList: ArrayList<CourseView>) {
+        courses= filteredList
+        notifyDataSetChanged()
     }
 }
-
-
-//
-//class RecyclerViewAdapter {
-//    private var courseList = arrayListOf<String>()
-//    private lateinit var context: Context
-//
-//    fun removeCourse(position: Int) {
-//        courseList.removeAt(position)
-////            notifyItemRemoved(position)
-//    }
-//
-//    fun getCourseAtPosition(position: Int) = courseList[position]
-//}
--- androidApp/src/main/java/com/example/coursefinder/androidApp/SearchView.kt
+++ androidApp/src/main/java/com/example/coursefinder/androidApp/SearchView.kt
@@ -2,8 +2,12 @@ package com.example.coursefinder.androidApp
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,38 +15,59 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coursefinder.androidApp.model.CourseView
+import java.util.Locale.filter
 
 class SearchViewActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var recyclerAdapter: RecyclerViewAdapter
+    private lateinit var recyclerViewAdapter: RecyclerViewAdapter
+    private lateinit var editText: EditText
+    private val courses= ArrayList<CourseView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        this.supportActionBar?.hide()
         setContentView(R.layout.searchview_activity)
+        editText = findViewById(R.id.editText)
 
+        val textWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                filter(s.toString());
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        }
 
-        val courses = ArrayList<CourseView>()
+        editText.addTextChangedListener(textWatcher)
 
-        for(i in 0 until 10){
+        for(i in 0 until 20){
             val item = CourseView("course $i", "description $i")
             courses += item
         }
 
-
-        recyclerView = findViewById<RecyclerView>(R.id.courseListView)
-        recyclerView.adapter = RecyclerViewAdapter(courses)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.setHasFixedSize(true)
+        createRecyclerView()
 
     }
 
-    fun populateRecyclerView(courses: List<String>) {
-        val courseList : ArrayList<String> = ArrayList()
-        for(course in courses){
-            courseList.add(course)
+    private fun filter(text: String) {
+        var filteredList = ArrayList<CourseView>()
+        for( course in courses){
+            if(course.title.toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(course);
+            }
         }
+        recyclerViewAdapter.filterList(filteredList)
 
     }
+
+    private fun createRecyclerView(){
+        recyclerView = findViewById(R.id.courseListView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerViewAdapter = RecyclerViewAdapter(this, courses)
+        recyclerView.adapter = recyclerViewAdapter;
+
+    }
+
 
 }
