09d4097c1ff531ca59d992e127f58a4caca5b327
Shani Yona <syona@uoguelph.ca>
2021-04-03 23:22:55 -0400

    navigating to searchview with args works

--- androidApp/src/main/java/com/example/coursefinder/androidApp/SearchView.kt
+++ androidApp/src/main/java/com/example/coursefinder/androidApp/SearchView.kt
@@ -11,19 +11,23 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coursefinder.androidApp.model.CourseView
+import androidx.navigation.fragment.navArgs
+import com.example.coursefinder.androidApp.course.SubscriptionFragmentArgs
+
 
 class SearchViewFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var recyclerViewAdapter: RecyclerViewAdapter
     private lateinit var editText: EditText
     private val courses= ArrayList<CourseView>()
+    private val args: SearchViewFragmentArgs by navArgs()
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                           savedInstanceState: Bundle?): View? {
         super.onCreate(savedInstanceState)
         var view = inflater.inflate(R.layout.searchview_fragment, container, false)
             editText = view.findViewById(R.id.editText)
-
 
         val textWatcher = object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
@@ -34,7 +38,7 @@ class SearchViewFragment : Fragment() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
         }
-
+        println(args.searchType)
         editText.addTextChangedListener(textWatcher)
 
         for(i in 0 until 20){
--- androidApp/src/main/java/com/example/coursefinder/androidApp/course/Select.kt
+++ androidApp/src/main/java/com/example/coursefinder/androidApp/course/Select.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.example.coursefinder.androidApp.R
 import com.example.coursefinder.androidApp.databinding.SelectSearchTypeBinding
@@ -30,11 +29,15 @@ class SelectSearchFragment: Fragment(R.layout.select_search_type), SelectDelegat
     }
 
     override fun searchByCourseCodeButtonAction() {
-        TODO("Not yet implemented")
+       findNavController().navigate(
+            SelectSearchFragmentDirections.actionSelectSearchToSearchView("courseCode")
+        )
     }
 
     override fun searchByCourseNameButtonAction() {
-        TODO("Not yet implemented")
+        findNavController().navigate(
+            SelectSearchFragmentDirections.actionSelectSearchToSearchView("courseName")
+        )
     }
 
     private fun SelectSearchTypeBinding.setContent(model: SelectModel) {
@@ -44,7 +47,7 @@ class SelectSearchFragment: Fragment(R.layout.select_search_type), SelectDelegat
         this.primaryButton.setOnClickListener { model.searchByCourseCodeButtonAction() }
 
         this.secondaryButton.text = model.searchByCourseNameButtonText
-        this.secondaryButton.setOnClickListener { model.searchByCourseCodeButtonAction() }
+        this.secondaryButton.setOnClickListener { model.searchByCourseNameButtonAction() }
 
         // TODO - remove from here down when the subscription fragment is wired up
         val btn = Button(context)
