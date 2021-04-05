e487fe85d85bca3f051099dfa43914f9c1c4c417
Josh Samuels <joshsamuels04@gmail.com>
2021-04-05 19:25:49 -0400

    made courses livedata and setup some data binding boilerplate

--- shared/src/commonMain/kotlin/com/example/coursefinder/shared/course/SearchCourse.kt
+++ shared/src/commonMain/kotlin/com/example/coursefinder/shared/course/SearchCourse.kt
@@ -1,36 +1,26 @@
 package com.example.coursefinder.shared.course
 
+import com.example.coursefinder.shared.model.Course
+import dev.icerock.moko.mvvm.livedata.MutableLiveData
+import dev.icerock.moko.mvvm.livedata.readOnly
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
-
-enum class SearchCourseBy {
-    COURSE_CODE,
-    COURSE_NAME
-}
-
-data class SearchCourseRow(
-    val courseIdentifier: String,
-    val selectRowAction: () -> Unit
-)
-
-data class SearchCourseModel(
-    val rows: List<SearchCourseRow>
-)
 
 interface SearchCourseDelegate {
     fun showCourseDetails(courseCode: String)
 }
 
 class SearchCourseViewModel(private val delegate: SearchCourseDelegate?): ViewModel() {
-    data class Course(val courseCode: String, val courseName: String)
-    val courses = listOf(
-        Course("CIS*1000", "Intro to Programming"),
-        Course("CIS*1500", "Programming"),
-        Course("CIS*4250", "Soft design"),
-        Course("ACCT*1220", "Intro to accounting"),
-        Course("ACCT*4420", "accounting"),
+    private val _courses = listOf(
+        MutableLiveData(Course("CIS*1000", "Intro to Programming")),
+        MutableLiveData(Course("CIS*1500", "Programming")),
+        MutableLiveData(Course("CIS*4250", "Soft design")),
+        MutableLiveData(Course("ACCT*1220", "Intro to accounting")),
+        MutableLiveData( Course("ACCT*4420", "accounting")),
     )
 
+    val courses = _courses.map { it.readOnly() }
+
     fun rowAction(index: Int) {
-        delegate?.showCourseDetails(courses[index].courseCode)
+        delegate?.showCourseDetails(_courses[index].value.courseCode)
     }
 }
---
+++ shared/src/commonMain/kotlin/com/example/coursefinder/shared/model/Course.kt
@@ -0,0 +1,3 @@
+package com.example.coursefinder.shared.model
+
+data class Course(val courseCode: String, val courseName: String)
